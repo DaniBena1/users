74578a866330040a7385fc5a4bfee0ccb4a89641
DANIEL BEN AVRAHAM <danibenavraham@gmail.com>
2022-12-26 22:16:08 +0200

    add contact to table

--- src/App.js
+++ src/App.js
@@ -1,27 +1,103 @@
 import { useState } from "react";
 import "./App.css";
-import FetchFromFile from "./FetchFromField.js";
-import FetchFromURL from "./FetchFromURL.js";
+import data from './userData.json';
+import {nanoid} from 'nanoid';
 
-function App() {
-  const [bool, setBool] = useState(true);
-  const [btnTxt, setBtnTxt] = useState("Fetch Data From URL");
-  function showFetchData() {
-    setBool(!bool);
-    if (bool === true) {
-      setBtnTxt("Fetch Data From JSON File");
-    } else {
-      setBtnTxt("Fetch Data From URL");
+const App = () => {
+
+  const[contacts, setContacts] = useState(data)
+  const[addFormData, setAddFormData] = useState({
+    name:'',
+    address:'',
+    phone:'',
+    email:'',
+  })
+
+  const handleAddFormChange = (event) =>{
+    event.preventDefault()
+
+    const fieldName = event.target.getAttribute('name')
+    const fieldValue = event.target.value
+
+    const newFormData = {...addFormData}
+    newFormData[fieldName] = fieldValue
+
+
+    setAddFormData(newFormData)
+  }
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault()
+    const newContact = {
+      id: nanoid(),
+      name: addFormData.name,
+      address: addFormData.address,
+      phone: addFormData.phone,
+      email: addFormData.email,
     }
+
+    const newContacts = [...contacts, newContact]
+    setContacts(newContacts)
   }
   return (
     <div className="App">
-      <button className="submit-btn" onClick={showFetchData}>
-        {btnTxt}
-      </button>
-      {bool === true ? <FetchFromFile /> : <FetchFromURL />}
+      <h2>Add a Contact</h2>
+
+      <form onSubmit={handleAddFormSubmit}>
+        <input type="text" 
+        name='name' 
+        required = 'required' 
+        placeholder='Enter a name'
+        onChange={handleAddFormChange}
+        />
+        <input type= "text" 
+        name= "address" 
+        required = "required" 
+        placeholder="Enter an address"
+        onChange={handleAddFormChange}
+        />
+        <input type="text" 
+        name= "phone"
+        required = 'required' 
+        placeholder="Enter a phone number"
+        onChange={handleAddFormChange}
+        />
+        <input type= "email" 
+        name= "email"
+        required = 'required' 
+        placeholder="Enter an email"
+        onChange={handleAddFormChange}
+        />
+        <button type="Submit">Add</button>
+      </form>
+
+
+      <h1>contact</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact)=>(
+          <tr>
+            <td> {contact.name}</td>
+            <td>{contact.address.city}</td>
+            <td>{contact.phone}</td>
+            <td>{contact.email}</td>
+          </tr>
+          ))}
+
+        </tbody>
+      </table>
+
+      
     </div>
-  );
+  )
 }
 
 export default App;
